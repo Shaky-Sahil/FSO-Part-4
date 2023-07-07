730fab613c0cc940c832198a3f4329377e6c5bd3
Sahil Shaikh <sahilpk81@gmail.com>
2023-07-07 18:16:34 +0530

    updated project structure

---
+++ models/blogdb.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+.then(()=>console.log("successfully connected to db"))
+.catch(()=>console.log("error while connecting"))
+
+module.exports = Blog;
