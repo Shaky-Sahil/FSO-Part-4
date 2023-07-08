20381ab43a8c10519e3cc0f5017391e66014f70b
Sahil Shaikh <sahilpk81@gmail.com>
2023-07-08 13:43:31 +0530

    ex 4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,23 @@
 const dummy = (blogs) => 1;
+
 const totalLikes = (blogs) => {
   const total = blogs.reduce((sum, b) => sum += b.likes, 0);
   return total;
 };
+
+const favoriteBlog = (blogs) => {
+  let maxLikes = 0;
+  let maxIndex=0;
+  blogs.forEach((b,i) => {
+    if (b.likes > maxLikes) {
+      maxLikes = b.likes;
+      maxIndex = i;
+    }
+  });
+  return blogs[maxIndex];
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
