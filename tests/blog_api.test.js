14e5ef547805527751cb45ade49279cf2ce3adf4
Sahil Shaikh <sahilpk81@gmail.com>
2023-07-13 17:58:25 +0530

    ex 4.8 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,20 +1,26 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blogdb');
+const helper = require('./test_helper');
 
-const api = supertest(app)
+const api = supertest(app);
 
-// beforAll(async () => {
-//     const initialBlogs = 
-// })
+beforeAll(async () => {
+  await Blog.deleteMany({});
+  const blogObjects = helper.initialBlogs.map((b)=> new Blog(b))
+  const promiseArray = blogObjects.map((b)=> b.save())
+  await Promise.all(promiseArray)
+}, 20000);
 
 test('notes are returned as json', async () => {
-  await api
+  const response = await api
     .get('/api/blogs')
     .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+    .expect('Content-Type', /application\/json/);
+  expect(response.body).toHaveLength(6);
+}, 20000);
 
 afterAll(async () => {
-  await mongoose.connection.close()
-})
+  await mongoose.connection.close();
+});
